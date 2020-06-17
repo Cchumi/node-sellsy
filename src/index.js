880fff7b4bcd85b809d92bf20a6abee1dfb69b4f
cchumi <cchumi.hacks@gmail.com>
2020-06-17 18:04:10 +0200

    update

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ Sellsy.prototype.api = function({ method = 'Infos.getInfos', params = {}} = {}) 
   return new Promise((resolve, reject) => {
     const postData = {
       request: 1,
-      io_mode: 'json',
+      io_mode: 'jsons',
       do_in: JSON.stringify({
         method: method,
         params: params
